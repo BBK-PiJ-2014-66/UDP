f65def03e9db02ee769c81d3625f955f2b214f88
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-20 19:03:59 +0100

    Start work on the server. Initially just wait and count connections.

---
+++ src/uk/fictitiousurl/audiorelay/Server.java
@@ -0,0 +1,44 @@
+package uk.fictitiousurl.audiorelay;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Server for client-application where clients send or receive looping audio.
+ * 
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ */
+public class Server {
+
+	/**
+	 * the port number for the TCP connection used for signalling
+	 */
+	private static int PORTSIGNAL = 7777;
+
+	public static void main(String[] args) {
+		Server theServer = new Server();
+		theServer.launch();
+	}
+
+	public void launch() {
+		try {
+			ServerSocket signalSocket = new ServerSocket(PORTSIGNAL);
+			int connectionNumb = 0;
+			while (true) {
+				System.out.println("log: waiting for client " + connectionNumb
+						+ " to connect");
+				Socket clientSocket = signalSocket.accept();
+				System.out.println("log: client " + connectionNumb
+						+ " has connected");
+				// TODO launch thread
+
+				connectionNumb++;
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
