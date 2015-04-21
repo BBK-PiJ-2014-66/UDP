fa95c8e2ec979119d190d09d5822a2010fafc16a
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-21 09:13:22 +0100

    remove ununused import

--- src/uk/fictitiousurl/audiorelay/Server.java
+++ src/uk/fictitiousurl/audiorelay/Server.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import uk.fictitiousurl.development.KKMultiServerThread;
-
 /**
  * Server for client-application where clients send or receive looping audio.
  * 
