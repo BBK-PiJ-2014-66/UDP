11e6af60810533ba74b96e188ffa8982bbd48d37
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-21 12:28:57 +0100

    Move clientSocket into a Try-With-resources statement gets rid of an ecilpse warning

--- src/uk/fictitiousurl/audiorelay/Client.java
+++ src/uk/fictitiousurl/audiorelay/Client.java
@@ -37,49 +37,36 @@ public class Client {
 	}
 
 	public void launch(String hostname) {
-        // First setup the signal communication socket, writer and reader
-		Socket clientSocket;
+		// First setup the signal communication socket, writer and reader
 		PrintWriter toServer;
 		BufferedReader fromServer;
-		try {
-			clientSocket = new Socket(hostname, Ports.SIGNAL);
-			toServer = new PrintWriter(clientSocket.getOutputStream(),
-					true);
+		try (Socket clientSocket = new Socket(hostname, Ports.SIGNAL);) {
+			toServer = new PrintWriter(clientSocket.getOutputStream(), true);
 			fromServer = new BufferedReader(new InputStreamReader(
 					clientSocket.getInputStream()));
+
+			System.out.println("log: connected to host " + hostname
+					+ " port number " + Ports.SIGNAL);
+			// First ask for ID
+			System.out.println("log: asking for ID");
+			toServer.println("askID");
+			// get the info back from the Server
+			String back = fromServer.readLine();
+			System.out.println("log: got back " + back);
+
 		} catch (UnknownHostException ex) {
 			System.err.println("ERROR UnknownHostException caught "
 					+ "trying to create new socket on port " + Ports.SIGNAL
 					+ "   details: " + ex.getMessage());
 			return;
 		} catch (IOException ex) {
-			System.err.println("ERROR IOException caught "
-					+ "trying to create new socket/reader/writer on port " + Ports.SIGNAL
-					+ "   details: " + ex.getMessage());
-			return;
-
-		}
-		System.out.println("log: connected to host " + hostname
-				+ " port number " + Ports.SIGNAL);
-
-		try {
-	    	// First ask for ID
-			System.out.println("log: asking for ID");
-	        toServer.println("askID");
-	        // get the info back from the Server
-	        String back = fromServer.readLine();
-			System.out.println("log: got back " + back);
-		
-		} catch (IOException ex) {
-			System.err.println("ERROR IOException caught "
-					+ "reading/writing to port " + Ports.SIGNAL
-					+ "   details: " + ex.getMessage());
+			System.err
+					.println("ERROR IOException caught "
+							+ "trying to create new socket/reader/writer or read/write to port "
+							+ Ports.SIGNAL + "   details: " + ex.getMessage());
 			return;
 
 		}
 
-
-		
 	}
-
 }
