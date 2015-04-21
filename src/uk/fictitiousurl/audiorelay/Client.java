99901880e3708ea658e73d475f6985604a2978da
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-21 12:21:06 +0100

    Client asks server for its ID - does not yet get a reply.

--- src/uk/fictitiousurl/audiorelay/Client.java
+++ src/uk/fictitiousurl/audiorelay/Client.java
@@ -1,6 +1,9 @@
 package uk.fictitiousurl.audiorelay;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -34,9 +37,16 @@ public class Client {
 	}
 
 	public void launch(String hostname) {
+        // First setup the signal communication socket, writer and reader
 		Socket clientSocket;
+		PrintWriter toServer;
+		BufferedReader fromServer;
 		try {
 			clientSocket = new Socket(hostname, Ports.SIGNAL);
+			toServer = new PrintWriter(clientSocket.getOutputStream(),
+					true);
+			fromServer = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
 		} catch (UnknownHostException ex) {
 			System.err.println("ERROR UnknownHostException caught "
 					+ "trying to create new socket on port " + Ports.SIGNAL
@@ -44,15 +54,31 @@ public class Client {
 			return;
 		} catch (IOException ex) {
 			System.err.println("ERROR IOException caught "
-					+ "trying to create new socket on port " + Ports.SIGNAL
+					+ "trying to create new socket/reader/writer on port " + Ports.SIGNAL
 					+ "   details: " + ex.getMessage());
 			return;
 
 		}
 		System.out.println("log: connected to host " + hostname
 				+ " port number " + Ports.SIGNAL);
+
+		try {
+	    	// First ask for ID
+			System.out.println("log: asking for ID");
+	        toServer.println("askID");
+	        // get the info back from the Server
+	        String back = fromServer.readLine();
+			System.out.println("log: got back " + back);
 		
-		
+		} catch (IOException ex) {
+			System.err.println("ERROR IOException caught "
+					+ "reading/writing to port " + Ports.SIGNAL
+					+ "   details: " + ex.getMessage());
+			return;
+
+		}
+
+
 		
 	}
 
