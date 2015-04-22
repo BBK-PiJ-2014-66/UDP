4b4bc2a8701610f1358598acf4613f19caeee954
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-22 13:59:22 +0100

    Log the port numbers for packets.
    Using UDP a single server thread can work with multiple connecting
    clients. Run the server and connect from 3 different clients (each run
    from a separate terminal window):
    
    Server log =============================
    
    $ java -cp bin uk.fictitiousurl.development.Server 
    RECEIVED: '1st client' from IP: /127.0.0.1 port: 43099
    RECEIVED: '2nd client' from IP: /127.0.0.1 port: 33610
    RECEIVED: '3rd client' from IP: /127.0.0.1 port: 36892
    RECEIVED: '1st client again' from IP: /127.0.0.1 port: 43099
    RECEIVED: '2nd client again' from IP: /127.0.0.1 port: 33610
    RECEIVED: '3rd client again' from IP: /127.0.0.1 port: 36892
    
    1st client log ===========================
    $ java -cp bin uk.fictitiousurl.development.Client 
    To send? 1st client
    FROM SERVER: '1ST CLIENT' SocketAddress /127.0.0.1:2000
    To send? 1st client again
    FROM SERVER: '1ST CLIENT AGAIN' SocketAddress /127.0.0.1:2000
    To send? 
    
    2nd client log ============================
    $ java -cp bin uk.fictitiousurl.development.Client 
    To send? 2nd client
    FROM SERVER: '2ND CLIENT' SocketAddress /127.0.0.1:2000
    To send? 2nd client again
    FROM SERVER: '2ND CLIENT AGAIN' SocketAddress /127.0.0.1:2000
    To send? 
    
    Conclusion: for each client there is a distinct return port number to
    send return packets to.

--- src/uk/fictitiousurl/development/Client.java
+++ src/uk/fictitiousurl/development/Client.java
@@ -6,30 +6,37 @@ import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String args[]) throws Exception {
-        try (Scanner sc = new Scanner(System.in)) {
+	public static void main(String args[]) throws Exception {
+		try (Scanner sc = new Scanner(System.in)) {
 
-            try (DatagramSocket clientSocket = new DatagramSocket()) {
-                InetAddress IPAddress = InetAddress.getByName("localhost");
+			try (DatagramSocket clientSocket = new DatagramSocket()) {
+				InetAddress IPAddress = InetAddress.getByName("localhost");
 
-                do {
-                    byte[] receiveData = new byte[1024];
+				do {
+					byte[] receiveData = new byte[1024];
 
-                    System.out.print("To send? ");
-                    String sentence = sc.nextLine();
+					System.out.print("To send? ");
+					String sentence = sc.nextLine();
 
-                    if ("STOP".equals(sentence)) break;
+					if ("STOP".equals(sentence))
+						break;
 
-                    byte[] sendData = sentence.getBytes();
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2000);
-                    clientSocket.send(sendPacket);
+					byte[] sendData = sentence.getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendData,
+							sendData.length, IPAddress, 2000);
+					clientSocket.send(sendPacket);
 
-                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                    clientSocket.receive(receivePacket);
-                    String modifiedSentence = new String(receivePacket.getData());
-                    System.out.println("FROM SERVER:" + modifiedSentence);
-                } while (true);
-            }
-        }
-    }
+					DatagramPacket receivePacket = new DatagramPacket(
+							receiveData, receiveData.length);
+					clientSocket.receive(receivePacket);
+					String modifiedSentence = new String(
+							receivePacket.getData());
+					System.out.println("FROM SERVER: '" + modifiedSentence
+							+ "'" + " SocketAddress "
+							+ receivePacket.getSocketAddress());
+
+				} while (true);
+			}
+		}
+	}
 }
--- src/uk/fictitiousurl/development/Server.java
+++ src/uk/fictitiousurl/development/Server.java
@@ -4,26 +4,37 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/**
+ * Example UDP echo server
+ * 
+ * downloaded from https://bitbucket.org/keithmannock/pij14-15/raw/657d73
+ * cc0adf1fb9bec1a5712b4966274fb9926a/network/src/udp/Server.java
+ * 
+ * @author Keith Mannock (modified a bit by Oliver).
+ *
+ */
 public class Server {
-    public static void main(String args[]) throws Exception {
-        DatagramSocket serverSocket = new DatagramSocket(2000);
+	public static void main(String args[]) throws Exception {
+		DatagramSocket serverSocket = new DatagramSocket(2000);
 
+		while (true) {
+			byte[] receiveData = new byte[1024];
+			byte[] sendData;
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+					receiveData.length);
+			serverSocket.receive(receivePacket);
+			String sentence = new String(receivePacket.getData());
 
-        while (true) {
-            byte[] receiveData = new byte[1024];
-            byte[] sendData;
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            String sentence = new String(receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
+			InetAddress IPAddress = receivePacket.getAddress();
+			int port = receivePacket.getPort();
 
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port = receivePacket.getPort();
-            String capitalizedSentence = sentence.toUpperCase();
-            sendData = capitalizedSentence.getBytes();
-            DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            serverSocket.send(sendPacket);
-        }
-    }
+			System.out.println("RECEIVED: '" + sentence + "' from IP: "
+					+ IPAddress + " port: " + port);
+			String capitalizedSentence = sentence.toUpperCase();
+			sendData = capitalizedSentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData,
+					sendData.length, IPAddress, port);
+			serverSocket.send(sendPacket);
+		}
+	}
 }
