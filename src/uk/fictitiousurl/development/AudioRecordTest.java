79b179b134c6a26bf827a3c3df7f79c1b5d72fbe
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-26 19:27:10 +0100

    Test play back of 9 seconds of Bach Cello Suite number 1. Works OK apart from slight click see https://github.com/BBK-PiJ-2014-66/UDP/wiki/Development-work#test-audio-data

--- src/uk/fictitiousurl/development/AudioRecordTest.java
+++ src/uk/fictitiousurl/development/AudioRecordTest.java
@@ -1,10 +1,10 @@
 package uk.fictitiousurl.development;
 
-
-
 import uk.fictitiousurl.audiorelay.AudioRecord;
+
 /**
- * Simple test of the AudioRecord Object. Open an audio file and play the sound
+ * Simple test of the AudioRecord Object. Open an audio file and play the sound.
+ * Extended to play 9 seconds of Bach Cello Suite Number 1.
  * 
  * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
  *
@@ -12,9 +12,20 @@ import uk.fictitiousurl.audiorelay.AudioRecord;
 public class AudioRecordTest {
 
 	public static void main(String[] args) {
-		AudioRecord soundC = new AudioRecord("./audioFiles/C.aiff");
-		// play the sound
-		soundC.play();
+		
+		AudioRecord Bach[] = new AudioRecord[9];
+		for (int ic = 0; ic < 9; ic++) {
+			Bach[ic] = new AudioRecord("./audioFiles/Bach" + (ic + 1) + ".wav");
+		}
+		
+		System.out.println("9 seconds of Bach loaded, format is " + Bach[0].getAudioFormat());
+
+		// test playing back - looping 3 times
+		for (int ic = 0; ic < 3; ic++) {
+			for (AudioRecord arit : Bach) {
+				arit.play();
+			}
+		}
 	}
 
 }
