1ff354bc8511bc20a82ccec9935c5cec6b52d334
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-26 17:56:21 +0100

    To make sound handling easier create an AudioRecord object to handle reading and playing back sound.

---
+++ src/uk/fictitiousurl/audiorelay/AudioRecord.java
@@ -0,0 +1,74 @@
+package uk.fictitiousurl.audiorelay;
+
+import static uk.fictitiousurl.audiorelay.AudioUtils.audioFileFormat;
+import static uk.fictitiousurl.audiorelay.AudioUtils.audioFileToByteArray;
+import static uk.fictitiousurl.audiorelay.AudioUtils.playBack;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ * Object to store an audio "record": a short audio recording read from a file,
+ * or transmitted using UDP.
+ *
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ */
+public class AudioRecord {
+	private byte[] bytes;
+	private AudioFormat audioFormat;
+
+	/**
+	 * construct audio record from a file
+	 * 
+	 * @param filename
+	 *            the filename including any path
+	 * @throws RuntimeException
+	 *             if there is a problem opening or reading from the file
+	 */
+	public AudioRecord(String fileName) {
+		try {
+			bytes = audioFileToByteArray(fileName);
+			audioFormat = audioFileFormat(fileName);
+		} catch (UnsupportedAudioFileException | IOException ex) {
+			throw new RuntimeException("ERROR reading audio record from file "
+					+ fileName + " details: " + ex.getMessage());
+		}
+
+	}
+
+	/**
+	 * Getter for the byte array.
+	 * 
+	 * @return the bytes
+	 */
+	public byte[] getBytes() {
+		return bytes;
+	}
+
+	/**
+	 * Getter for the audio Format.
+	 * 
+	 * @return the audioFormat
+	 */
+	public AudioFormat getAudioFormat() {
+		return audioFormat;
+	}
+
+	/**
+	 * play back the audio record via speakers.
+	 */
+	public void play() {
+		try {
+			playBack(audioFormat, bytes);
+		} catch (LineUnavailableException ex) {
+			throw new RuntimeException(
+					"ERROR playing audio via speakers. Details: "
+							+ ex.getMessage());
+		}
+
+	}
+
+}
