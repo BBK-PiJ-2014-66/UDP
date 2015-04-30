c68a411bd955b27a4a5cab5ca7204cf5f315ad6f
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-30 16:10:38 +0100

    Revise JavaDoc comments to improve understandability (hopefully).

--- src/uk/fictitiousurl/audiorelay/Mode.java
+++ src/uk/fictitiousurl/audiorelay/Mode.java
@@ -1,9 +1,16 @@
 package uk.fictitiousurl.audiorelay;
 
 /**
- * Client's mode. 
+ * Client's mode.
  */
 public enum Mode {
-	SENDER, 
-	RECEIVER  
+	/**
+	 * Client is a sender so will send audio to the server.
+	 */
+	SENDER,
+	/**
+	 * Client is a receiver so will receive audio from the server and play it
+	 * back.
+	 */
+	RECEIVER
 }
