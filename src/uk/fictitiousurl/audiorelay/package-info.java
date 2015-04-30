5742b35794e6a86ececc5cc49829e4102bd70a5a
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-30 15:51:46 +0100

    package-info describing package for JavaDoc

---
+++ src/uk/fictitiousurl/audiorelay/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Provides a simple client-server application where multiple clients can 
+ * connect to a server that can relay audio from a sending client to the others.
+ * <p>
+ * Please see Project GitHub page
+ * <a href="https://github.com/BBK-PiJ-2014-66/UDP" target="_blank">https://github.com/BBK-PiJ-2014-66/UDP</a>
+ * and wiki page <a href="https://github.com/BBK-PiJ-2014-66/UDP/wiki" target="_blank">https://github.com/BBK-PiJ-2014-66/UDP/wiki</a> 
+ * for a detailed description.
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ */
+package uk.fictitiousurl.audiorelay;
