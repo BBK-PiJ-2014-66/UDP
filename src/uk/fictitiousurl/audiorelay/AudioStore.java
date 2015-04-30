eb78566477fb4d6032726f93d0abf7c3c29c8fcf
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-30 13:12:39 +0100

    Revise JavaDoc comments to improve understandability (hopefully).

--- src/uk/fictitiousurl/audiorelay/AudioStore.java
+++ src/uk/fictitiousurl/audiorelay/AudioStore.java
@@ -1,16 +1,24 @@
 package uk.fictitiousurl.audiorelay;
 
 /**
- * Store used by Server to enable chunk of audio data to be relayed
+ * Store used by Server to enable chunk of audio data to be relayed. In the
+ * future this could be extended to store a number of chunks to provide a
+ * buffer.
  * 
  * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
  */
 public class AudioStore {
+
+	/**
+	 * Currently the AudioStore stores a single "chunk" of audio.
+	 */
 	private AudioRecord storedAudio = null;
 
 	/**
-	 * @param storedAudio
-	 *            the storedAudio to set
+	 * Setter to store an audio chunk.
+	 * 
+	 * @param supplied
+	 *            the audio chunk to store.
 	 */
 	public synchronized void setStoredAudio(AudioRecord supplied) {
 		storedAudio = new AudioRecord(supplied.getAudioFormat(),
@@ -18,9 +26,9 @@ public class AudioStore {
 	}
 
 	/**
-	 * Getter Return a clone of the stored audio
+	 * Getter provides a clone of the stored audio.
 	 * 
-	 * @return the storedAudio or null if nothing stored;
+	 * @return the a clone of the stored audio or null if nothing is stored;
 	 */
 	public synchronized AudioRecord getStoredAudio() {
 		AudioRecord clone = null;
