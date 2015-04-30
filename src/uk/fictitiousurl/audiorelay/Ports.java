216a7646bfdbbf5da47475e0fa5eafb76c57b49e
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-04-30 16:14:22 +0100

    A private constructor to make Ports uninstantiable.... Revise JavaDoc comments to improve understandability (hopefully).

--- src/uk/fictitiousurl/audiorelay/Ports.java
+++ src/uk/fictitiousurl/audiorelay/Ports.java
@@ -7,14 +7,22 @@ package uk.fictitiousurl.audiorelay;
  * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
  */
 public class Ports {
+
+	/**
+	 * Ports is an uninstantiable class.
+	 */
+	private Ports() {
+		throw new UnsupportedOperationException("Uninstantiable class");
+	}
+
 	/**
 	 * the port number for the TCP connection used for signalling
 	 */
 	public static int SIGNAL = 7777;
-	
+
 	/**
-	 * the starting port number for UDP connections.
-	 * client id=0 will user this port, id=1 will use next etc.  
+	 * the starting port number for UDP connections. client id=0 will user this
+	 * port, id=1 will use the next port etc.
 	 */
 	public static int UDPSTART = 7780;
 
